bca2ed8f789b2f407dd0959c3c5b30339f581d59
AryaSwati321 <aryaswati1907@gmail.com>
2025-05-08 10:29:30 +0530

    Initial Commit

---
+++ 2b/2b.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+console.log("Arya , Here");
+console.log("This is a Docker Container");
+
+console.log("My Details:");
+console.log("Roll No.: 307B003");
+console.log("Division: 2A");
+
+
